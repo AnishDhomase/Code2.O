0e35a41b37f7d7e5c0d70ca0f53840e00c831fc5
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-03-15 15:37:24 +0530

    Time: 12 ms (83.78%) | Memory: 26.5 MB (55.36%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,23 +2,36 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n, 0);
-        int indOdZero=-1, zeroes = 0, multiWithoutZeroes = 1;
-        for(int i=0; i<n; i++){
-            if(nums[i]==0){    
-                zeroes++;
-                indOdZero = i;
-            }
-            else        multiWithoutZeroes *= nums[i];
-        }
-        if(zeroes){
-            if(zeroes==1)
-                ans[indOdZero] = multiWithoutZeroes;
-            return ans;
-        }
-        for(int i=0; i<n; i++)
-            ans[i] = multiWithoutZeroes / nums[i];
+        vector<int> ans(n,1);
 
+        for(int i=1; i<n; i++)
+            ans[i] = ans[i-1] * nums[i-1];   
+        
+        int rightProd = 1;
+        for(int i=n-2; i>=0; i--){
+            rightProd *= nums[i+1];
+            ans[i] *= rightProd;  
+        }
+        
         return ans;
+        // int n = nums.size();
+        // vector<int> ans(n, 0);
+        // int indOdZero=-1, zeroes = 0, multiWithoutZeroes = 1;
+        // for(int i=0; i<n; i++){
+        //     if(nums[i]==0){    
+        //         zeroes++;
+        //         indOdZero = i;
+        //     }
+        //     else        multiWithoutZeroes *= nums[i];
+        // }
+        // if(zeroes){
+        //     if(zeroes==1)
+        //         ans[indOdZero] = multiWithoutZeroes;
+        //     return ans;
+        // }
+        // for(int i=0; i<n; i++)
+        //     ans[i] = multiWithoutZeroes / nums[i];
+
+        // return ans;
     }
 };
