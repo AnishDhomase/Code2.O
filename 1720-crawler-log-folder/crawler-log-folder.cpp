36d54e17e3f21a84983f540e8149c37a7a70cd78
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-07-10 12:09:54 +0530

    Time: 4 ms (67.13%) | Memory: 14 MB (61.11%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int level = 0;
+        for(auto log: logs){
+            if(log == "../"){
+                if(level > 0)   level --;
+            }  
+            else if(log != "./")
+                level ++;
+        }
+        return level;
+    }
+};
