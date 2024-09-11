130ef3916e6c3223f2809d59fd1d14788f49c216
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-09-11 23:37:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (44.87%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,27 +1,11 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int sBitsArr[32]={0}, gBitsArr[32]={0};
-        int sTemp=0, gTemp=0;
-        for(int i=0; i<32; i++){
-            if(sTemp == start)  break;
-            else if(sTemp + pow(2,31-i) <= start){
-                sTemp += pow(2,31-i);
-                sBitsArr[i] = 1; 
-            }
+        int xr = start ^ goal, cnt = 0;
+        while(xr) {
+            xr &= xr-1; 
+            cnt ++;
         }
-        for(int i=0; i<32; i++){
-            if(gTemp == goal)  break;
-            else if(gTemp + pow(2,31-i) <= goal){
-                gTemp += pow(2,31-i);
-                gBitsArr[i] = 1; 
-            }
-        }
-        int ct=0;
-        for(int i=0; i<32; i++){
-            if(sBitsArr[i] != gBitsArr[i])  ct++;
-        }
-        return ct;
-        
+        return cnt;  
     }
 };
