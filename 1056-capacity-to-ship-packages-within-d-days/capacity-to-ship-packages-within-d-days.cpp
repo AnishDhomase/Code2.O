3aebb92af5f5b5bf865b905dc9582dfee8c214a7
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-09-15 14:55:46 +0530

    Time: 35 ms (82.69%) | Memory: 33.9 MB (10.62%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,23 +1,23 @@
 class Solution {
-    bool isCapEnough(int cap, vector<int> weights, int days){
-        int d = 0, sum=0;
-        for(int i=0; i<weights.size(); i++){
-            if(weights[i] > cap)    return false;
-            sum += weights[i];
-            if(sum > cap){
-                i--;
-                d++;
-                sum=0;
+    bool isCapEnough(int cap, vector<int> &weights, int days){
+        int d = 1, sum=0;
+        for(auto w: weights){
+            if(sum + w <= cap)
+                sum += w;
+            else{
+                d ++;
+                sum = w;
             }
+
         }
-        return d+1 <= days;
+        return d <= days;
     }
 public:
     int shipWithinDays(vector<int>& weights, int days) {
         int sum = 0;
         for(auto w : weights)
             sum+=w;
-        int low = 1, high = sum, ans;
+        int low = *max_element(weights.begin(), weights.end()), high = sum, ans;
         while(low <= high){
             int mid = low + (high-low)/2;
             bool enough = isCapEnough(mid, weights, days);
