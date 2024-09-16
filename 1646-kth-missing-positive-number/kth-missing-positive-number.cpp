db4ba756c89b9ebdcfa31348a65f5937f9bac14f
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-09-16 13:48:38 +0530

    Time: 3 ms (71.45%) | Memory: 12.3 MB (84.76%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -2,25 +2,18 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         int n = arr.size();
-        int low = 1, high = n+k;
+        int low = 0, high = n-1;
         while(low <= high){
-            int mid = low + (high-low)/2;
-            int lb = lower_bound(arr.begin(), arr.end(), mid) - arr.begin();
-            int leftMissingCt;
-            if(lb<n && arr[lb] == mid)
-                leftMissingCt = mid - (lb+1);
-            else 
-                leftMissingCt = mid - lb;
-            if(leftMissingCt == k){
-                if(lb==n || arr[lb] != mid)
-                    return mid;
-                else    high = mid - 1;
-            }
-            else if(leftMissingCt < k)
-                low = mid + 1;
-            else
-                high = mid - 1;
+            int mid = (low + high) / 2;
+            int misssingCnt = arr[mid] - mid - 1;
+            if(misssingCnt < k) low = mid+1;
+            else                high = mid-1;
         }
-        return -1;
+        if(high != -1){
+            int afterThis = k - (arr[high] - high - 1);
+            return arr[high] + afterThis;
+        }
+        int beforeThis = (arr[low] - low - 1) - (k-1);
+        return arr[low] - beforeThis;
     }
 };
