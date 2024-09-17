d16eeca3f90ae9e2ecf43e104aee22e001f21a0e
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-09-17 21:44:29 +0530

    Time: 31 ms (41.73%) | Memory: 23.9 MB (23.71%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,22 +1,26 @@
 class Solution {
-    void bfs(int r, int c, vector<vector<int>> &vis, vector<vector<char>> grid){
-        int row = grid.size(), col = grid[0].size();
+    void bfs(int r, int c, vector<vector<bool>> &vis, vector<vector<char>> &grid){
+        int rows= grid.size(), cols = grid[0].size();
+
+        // init config
+        vis[r][c] = true;
         queue<pair<int,int>> Q;
-        vector<int> dX = {0,0,1,-1};
-        vector<int> dY = {1,-1,0,0};
         Q.push({r,c});
-        vis[r][c]++;
-        while(Q.size()){
-            auto node = Q.front();
+
+        // trav
+        vector<int> dY = {1, -1, 0, 0};
+        vector<int> dX = {0, 0, 1, -1};
+        while(Q.size() > 0){
+            auto currNode = Q.front();
+            int currY = currNode.first, currX = currNode.second;
             Q.pop();
-            int y = node.first, x = node.second;
             for(int i=0; i<4; i++){
-                int adjR = y + dY[i];
-                int adjC = x + dX[i];
-                if(0<=adjR && adjR<row && 0<=adjC && adjC<col && grid[adjR][adjC]=='1'){
-                    if(!vis[adjR][adjC]){
-                        Q.push({adjR,adjC});
-                        vis[adjR][adjC]++;
+                int adjY = currY + dY[i];
+                int adjX = currX + dX[i];
+                if(0 <= adjX && adjX < cols && 0 <= adjY && adjY < rows){
+                    if(grid[adjY][adjX] == '1' && !vis[adjY][adjX]){
+                        vis[adjY][adjX] = true;
+                        Q.push({adjY,adjX});
                     }
                 }
             }
@@ -24,17 +28,18 @@ class Solution {
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int row = grid.size(), col = grid[0].size(), ans=0;
-        vector<vector<int>> vis(row, vector<int>(col,0));
-        for(int r=0; r<row; r++){
-            for(int c=0; c<col; c++){
-                if(grid[r][c]=='1' && !vis[r][c]){
-                    ans++;
+        int rows= grid.size(), cols = grid[0].size(), cnt = 0;
+        vector<vector<bool>> vis(rows, vector<bool>(cols, false));
+
+        for(int r=0; r<rows; r++){
+            for(int c=0; c<cols; c++){
+                if(grid[r][c] == '1' && !vis[r][c]){
                     bfs(r, c, vis, grid);
+                    cnt ++;
                 }
             }
         }
-        return ans;
-
+        return cnt;
+        
     }
 };
