8ddbfd8ec56c117b4fb3526608b3e267b2e688d0
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-09-19 20:33:09 +0530

    Time: 2 ms (86.05%) | Memory: 12.8 MB (92.26%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -8,20 +8,28 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+    ListNode* revLL(ListNode *p, ListNode *c, ListNode *n){
+        if(!c)  return p;
+        c->next = p;
+        if(n)   return revLL(c, n, n->next);
+        return c;
+    }
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if(!head || !head->next)    return head;
-        ListNode *prev = head, *curr = head->next, *nxt = head->next->next;
-        while(curr){
-            curr -> next = prev;
-            prev = curr;
-            curr = nxt;
-            if(nxt)
-                nxt = nxt -> next;
-        }
-        head -> next = NULL;
-        head = prev;
-        return head;
+        // if(!head || !head->next)    return head;
+        // ListNode *prev = head, *curr = head->next, *nxt = head->next->next;
+        // while(curr){
+        //     curr -> next = prev;
+        //     prev = curr;
+        //     curr = nxt;
+        //     if(nxt)
+        //         nxt = nxt -> next;
+        // }
+        // head -> next = NULL;
+        // head = prev;
+        // return head;
+        if(!head)   return NULL;
+        return revLL(NULL, head, head->next);
     }
 };
