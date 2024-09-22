e680e4ef4f34d7ce856811d0cda62045c35b7d8d
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-09-22 16:48:01 +0530

    Time: 73 ms (97.12%) | Memory: 68.8 MB (97.09%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -2,32 +2,26 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int l=0, r=0, n = fruits.size(), maxL = 0;
-        pair<pair<int,int>,int> b1, b2;
-        b1 = b2 = {{-1,0},-1}; //{{type,cnt}, lastIndex}
+        pair<int,int> b1, b2;
+        b1 = b2 = {-1,-1}; //{type,lastIndex}
         while(r < n){
-            if(b1.first.first == -1){
-                b1 = {{fruits[r],1}, r};
-            }
-            else if(b1.first.first == fruits[r]){
-                b1.first.second ++;
-                b1.second = r;
-            }
-            else if(b2.first.first == -1){
-                b2 = {{fruits[r],1}, r};
-            }
-            else if(b2.first.first == fruits[r]){
-                b2.first.second ++;
-                b2.second = r;
-            }
+            if(b1.first == -1)
+                b1 = {fruits[r], r};
+            else if(b1.first == fruits[r])
+                b1.second = r; 
+            else if(b2.first == -1)
+                b2 = {fruits[r], r};            
+            else if(b2.first == fruits[r])
+                b2.second = r;            
             else{
                 maxL = max(maxL, r-l);
                 if(b1.second < b2.second){
                     l = b1.second + 1;
-                    b1 = {{fruits[r],1}, r};
+                    b1 = {fruits[r], r};
                 }
                 else{
                     l = b2.second + 1;
-                    b2 = {{fruits[r],1}, r};
+                    b2 = {fruits[r], r};
                 }
             }
             r ++;
