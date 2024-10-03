3357d64eed2dfcabfda976ea08655b365448e84a
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-10-03 09:00:35 +0530

    Time: 69 ms (5.75%) | Memory: 22.3 MB (5.11%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        // int ans = 0, n = s.length();
-        // for(int i=0; i<=n-3; i++){
-        //     int a, b, c;
-        //     a = b = c = 0;
-        //     for(int j=i; j<n; j++){
-        //         if(s[j]=='a')   a++;
-        //         else if(s[j]=='b')   b++;
-        //         else   c++;
-        //         if(a && b && c){
-        //             ans += n-j;
-        //             break;
-        //         }
-        //     }
-        //     if(!(a && b && c))
-        //         break;
-        // }
-        // return ans;
-
-        int ans = 0, n = s.length();
-        int a, b, c, i, j;
-        a = b = c = 0;
-        for(i=0, j=0; i<=n-3; i++){
-            for(; j<=n; j++){
-                if(a && b && c){
-                    ans += n-(j-1);
-                    if(s[i]=='a')   a--;
-                    else if(s[i]=='b')   b--;
-                    else   c--;
-                    break;
-                }
-                if(j==n)    break;
-                if(s[j]=='a')   a++;
-                else if(s[j]=='b')   b++;
-                else   c++;
+        int cnt = 0, l=0, r=0, n = s.length();
+        map<char,int> mp;
+        while(r < n){
+            mp[s[r]] ++;
+            while(mp.size() == 3){
+                cnt += (n-r);
+                mp[s[l]] --;
+                if(mp[s[l]] == 0)
+                    mp.erase(s[l]);
+                l ++;
             }
+            r ++;
         }
-        return ans;
+        return cnt;
     }
 };
