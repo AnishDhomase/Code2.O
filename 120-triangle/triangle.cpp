4d6e8a8f5b20e562f5abb0d8ccf6d25634f1bb9f
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-10-06 16:12:17 +0530

    Time: 4 ms (74.65%) | Memory: 11.8 MB (35.82%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -12,7 +12,18 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>> dp(n,vector<int>(n,1e9));
-        return getMinPath(0, 0, triangle, dp);
+        // return getMinPath(0, 0, triangle, dp);
+        for(int c=0; c<n; c++)
+            dp[n-1][c] = triangle[n-1][c];
+        for(int r=n-2; r>=0; r--){
+            for(int c=r; c>=0; c--){
+                int down = dp[r+1][c];
+                int diag = dp[r+1][c+1];
+
+                dp[r][c] = min(down, diag) + triangle[r][c];
+            }
+        }
+        return dp[0][0];
        
     }
 };
