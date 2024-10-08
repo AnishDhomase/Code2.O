0f31bf38b7f0f3edc5f8c39b316e43d5fd6ca7be
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-10-08 22:27:29 +0530

    Time: 38 ms (25.40%) | Memory: 26.3 MB (8.67%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,9 +1,9 @@
 class Solution {
     int getLCS(int i, int j, string &str1, string &str2, vector<vector<int>> &dp){
-        if(i<0 || j<0)  return 0;
+        if(i==0 || j==0)  return 0;
         if(dp[i][j] != -1)  return dp[i][j];
 
-        if(str1[i] == str2[j])
+        if(str1[i-1] == str2[j-1])
             return dp[i][j] = 1 + getLCS(i-1, j-1, str1, str2, dp);
         int p1 = getLCS(i-1, j, str1, str2, dp);
         int p2 = getLCS(i, j-1, str1, str2, dp);
@@ -13,7 +13,7 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length(), m = text2.length();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return getLCS(n-1, m-1, text1, text2, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return getLCS(n, m, text1, text2, dp);
     }
 };
