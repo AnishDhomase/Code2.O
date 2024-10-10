b28f732018b6769d9c7e820aefa3a7b3e657d466
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-10-10 16:34:46 +0530

    Time: 29 ms (60.37%) | Memory: 30 MB (56.75%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,9 +1,9 @@
 class Solution {
     int getLCS(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
-        if(i<0 || j<0)  return 0;
+        if(i==0 || j==0)  return 0;
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(s1[i] == s2[j])
+        if(s1[i-1] == s2[j-1])
             return dp[i][j] = 1 + getLCS(i-1, j-1, s1, s2, dp);
         int p1 = getLCS(i-1, j, s1, s2, dp);
         int p2 = getLCS(i, j-1, s1, s2, dp);
@@ -12,9 +12,23 @@ class Solution {
 public:
     int minInsertions(string s) {
         int n = s.length();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
         string rev = s;
         reverse(rev.begin(), rev.end());
-        return n - getLCS(n-1, n-1, s, rev, dp);
+        // return n - getLCS(n, n, s, rev, dp);
+        for(int i=0; i<=n; i++) dp[i][0] = 0;
+        for(int j=0; j<=n; j++) dp[0][j] = 0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s[i-1] == rev[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    continue;
+                }
+                int p1 = dp[i-1][j];
+                int p2 = dp[i][j-1];
+                dp[i][j] = max(p1, p2);
+            }
+        }
+        return n - dp[n][n];
     }
 };
