2c9eafb5d0f9f8552d9637d4f7a61c0574545f09
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-10-11 11:21:59 +0530

    Time: 14 ms (26.92%) | Memory: 12.9 MB (40.51%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,9 +1,9 @@
 class Solution {
     int getMinOps(int i, int j, string &word1, string &word2, vector<vector<int>> &dp){
-        if(i < 0)   return j+1; 
-        if(j < 0)   return i+1; 
+        if(i == 0)   return j; 
+        if(j == 0)   return i; 
         if(dp[i][j] != -1)  return dp[i][j];
-        if(word1[i] == word2[j])
+        if(word1[i-1] == word2[j-1])
             return dp[i][j] = getMinOps(i-1, j-1, word1, word2, dp);
         int ins = 1 + getMinOps(i, j-1, word1, word2, dp);
         int del = 1 + getMinOps(i-1, j, word1, word2, dp);
@@ -14,7 +14,24 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         int m = word1.length(), n = word2.length();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return getMinOps(m-1, n-1, word1, word2, dp);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        // return getMinOps(m, n, word1, word2, dp);
+        for(int j=0; j<=n; j++) dp[0][j] = j;
+        for(int i=0; i<=m; i++) dp[i][0] = i;
+
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                    continue;
+                }
+                int ins = 1 + dp[i][j-1];
+                int del = 1 + dp[i-1][j];
+                int repl = 1 + dp[i-1][j-1];
+                dp[i][j] = min(ins, min(del, repl));
+            }
+        }
+        return dp[m][n];
+        
     }
 };
