ed98ec3b7fea4d83922bbc3e8cbc578b847dcd27
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-10-14 20:45:24 +0530

    Time: 207 ms (34.73%) | Memory: 12.4 MB (7.12%) - LeetSync

---
+++ 312-burst-balloons/burst-balloons.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int getMaxCoins(int i, int j, vector<int> &baloon, vector<vector<int>> &dp){
+        if(i > j)   return 0;
+        if(dp[i][j] != -1)  return dp[i][j];
+
+        int maxC = 0;
+        for(int b=i; b<=j; b++){
+            int coins = baloon[i-1] * baloon[b] * baloon[j+1];
+            coins += getMaxCoins(i, b-1, baloon, dp);
+            coins += getMaxCoins(b+1, j, baloon, dp);
+            maxC = max(maxC, coins);
+        }
+        return dp[i][j] = maxC;
+    }
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        vector<int> baloon = {1};
+        for(auto num : nums)    baloon.push_back(num);
+        baloon.push_back(1);
+        return getMaxCoins(1, n, baloon, dp);
+    }
+};
