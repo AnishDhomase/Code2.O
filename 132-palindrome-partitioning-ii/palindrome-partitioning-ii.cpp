1c09aa1a66b6471289e322d91171d456bc96e13b
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-10-15 16:27:24 +0530

    Time: 1802 ms (5.18%) | Memory: 8.1 MB (68.13%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    bool isPal(int start, int end, string &s){
+        int n = s.length();
+        int i = start, j=end;
+        while(i<j){
+            if(s[i] != s[j])
+                return false;
+            i ++;
+            j --;
+        }
+        return true;
+    }
+    int getMinCuts(int i, string &s, vector<int> &dp){
+        int n = s.length();
+        if(i == n)   return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int minCuts = 1e9;
+        for(int p=i; p<n; p++){
+            if(!isPal(i, p, s))    continue;
+            int cuts = 1 + getMinCuts(p+1, s, dp);
+            minCuts = min(minCuts, cuts);
+        }
+        return dp[i] = minCuts;
+    }
+public:
+    int minCut(string s) {
+        int n = s.length();
+        vector<int> dp(n, -1);
+        return getMinCuts(0, s, dp) - 1;
+    }
+};
