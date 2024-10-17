98b250b51f733cb46908ba50ee9b7f629178ae7f
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-10-17 22:46:02 +0530

    Time: 91 ms (35.94%) | Memory: 36.5 MB (25.70%) - LeetSync

---
+++ 1343-dice-roll-simulation/dice-roll-simulation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    int mod = 1e9 + 7;
+    int getSeqCnt(int i, int lastDice, int consecutive, vector<int>& rollMax, vector<vector<vector<int>>> &dp){
+        if(i == 0)  return 1;
+        if(dp[i][lastDice][consecutive] != -1)
+            return dp[i][lastDice][consecutive];
+
+        int ways = 0;
+        for(int dice = 0; dice <= 5; dice ++){
+            if(dice != lastDice)
+                ways = (ways + getSeqCnt(i-1, dice, 1, rollMax, dp)) % mod;
+            else if(consecutive + 1 <= rollMax[lastDice])
+                ways = (ways + getSeqCnt(i-1, lastDice, consecutive + 1, rollMax, dp)) % mod;
+        }
+        return dp[i][lastDice][consecutive] = ways;
+    }
+public:
+    int dieSimulator(int n, vector<int>& rollMax) {
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(7, vector<int>(16, -1)));
+        return getSeqCnt(n, 6, 0, rollMax, dp);
+    }
+};
