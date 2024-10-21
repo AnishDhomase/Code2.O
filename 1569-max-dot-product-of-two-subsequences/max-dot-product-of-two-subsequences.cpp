0764802d556e88779eceb7b374ed38589eaaa390
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-10-21 13:42:35 +0530

    Time: 165 ms (5.95%) | Memory: 61.6 MB (5.51%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -1,12 +1,12 @@
 class Solution {
     int getMaxProd(int i, int j, int nonEmptySeq, vector<int>& nums1, vector<int>& nums2, vector<vector<vector<int>>> &dp){
-        if(i == -1 || j == -1){
+        if(i-1 == -1 || j-1 == -1){
             if(nonEmptySeq) return 0;
             return INT_MIN;
         }   
         if(dp[i][j][nonEmptySeq] != 1e9)   return dp[i][j][nonEmptySeq];
 
-        int way1 = nums1[i] * nums2[j] + getMaxProd(i-1, j-1, 1, nums1, nums2, dp);
+        int way1 = nums1[i-1] * nums2[j-1] + getMaxProd(i-1, j-1, 1, nums1, nums2, dp);
         int way2 = getMaxProd(i-1, j, nonEmptySeq, nums1, nums2, dp);
         int way3 = getMaxProd(i, j-1, nonEmptySeq, nums1, nums2, dp);
 
@@ -15,7 +15,9 @@ class Solution {
 public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int sz1 = nums1.size(), sz2 = nums2.size();
-        vector<vector<vector<int>>> dp(sz1, vector<vector<int>>(sz2, vector<int>(2, 1e9)));
-        return getMaxProd(sz1-1, sz2-1, 0, nums1, nums2, dp);
+        // vector<vector<vector<int>>> dp(sz1, vector<vector<int>>(sz2, vector<int>(2, 1e9)));
+        vector<vector<vector<int>>> dp(sz1+1, vector<vector<int>>(sz2+1, vector<int>(2, 1e9)));
+        return getMaxProd(sz1, sz2, 0, nums1, nums2, dp);
+
     }
 };
