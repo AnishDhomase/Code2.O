caddca1228adca4ff58bb8a634d28f6882961029
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-10-23 23:15:35 +0530

    Time: 3 ms (98.37%) | Memory: 43 MB (29.75%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,21 +1,23 @@
 class Solution {
-    bool getCanReach(int i, vector<int>& arr, vector<int> &vis){
+    bool getCanReach(int i, vector<int>& arr, vector<int> &vis, vector<int> &dp){
         int n = arr.size();
         if(arr[i] == 0) return true;
-        if(vis[i] == 1)  return false;
+        if(dp[i] != -1) return dp[i];
+
+        if(vis[i] == 1)  return dp[i] = false;
         vis[i] = 1;
 
         if(i - arr[i] >= 0)
-            if(getCanReach(i - arr[i], arr, vis))
-                return true;
+            if(getCanReach(i - arr[i], arr, vis, dp))
+                return dp[i] = true;
         if(i + arr[i] < n)
-            return getCanReach(i + arr[i], arr, vis);
-        return false;        
+            return dp[i] = getCanReach(i + arr[i], arr, vis, dp);
+        return dp[i] = false;        
     }
 public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
-        vector<int> vis(n, 0);
-        return getCanReach(start, arr, vis);
+        vector<int> vis(n, 0), dp(n, -1);
+        return getCanReach(start, arr, vis, dp);
     }
 };
