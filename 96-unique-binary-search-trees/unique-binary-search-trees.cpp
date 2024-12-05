ce184ab72b877f541acda53d0c46376a44eaf553
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-12-05 23:57:16 +0530

    Time: 1 ms (20.83%) | Memory: 8.8 MB (5.11%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,15 +1,17 @@
 class Solution {
-    int getTrees(int l, int r){
+    int getTrees(int l, int r, vector<vector<int>> &dp){
         if(l >= r) return 1;
+        if(dp[l][r] != -1)  return dp[l][r];
 
         int ways = 0;
         for(int par=l; par<=r; par++){
-            ways += getTrees(l, par-1) * getTrees(par+1, r); 
+            ways += getTrees(l, par-1, dp) * getTrees(par+1, r, dp); 
         }
-        return ways;
+        return dp[l][r] = ways;
     }
 public:
     int numTrees(int n) {
-        return getTrees(1, n);
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return getTrees(0, n-1, dp);
     }
 };
