a10c5629e2c0619c7e2fdf026d2233e99125692a
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-12-26 23:39:45 +0530

    Time: 994 ms (5.04%) | Memory: 11.5 MB (78.96%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,15 +1,15 @@
 class Solution {
-    int fun(int i, int sum, int t, vector<int>& nums){
-        if(i == nums.size()){
-            if(sum == t)  return 1;
-            return 0;
-        }
-        int p1 = fun(i+1, sum + nums[i], t, nums);
-        int p2 = fun(i+1, sum - nums[i], t, nums);
-        return p1 + p2;
+    int getWays(int i, int sum, vector<int> &nums, int target){
+        int n = nums.size();
+        if(i == n)  return sum == target;
+        // +
+        int ways = getWays(i+1, sum + nums[i], nums, target);
+        // -
+        ways += getWays(i+1, sum - nums[i], nums, target);
+        return ways;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return fun(0, 0, target, nums);
+        return getWays(0, 0, nums, target);
     }
 };
