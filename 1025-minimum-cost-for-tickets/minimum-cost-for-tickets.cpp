89fd38b3e72d104eef7c8faa13c619457297939a
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2024-12-31 09:20:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (32.35%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -20,7 +20,23 @@ class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        vector<int> dp(n, -1);
-        return getMinCost(0, days, costs, dp);
+        // vector<int> dp(n, -1);
+        // return getMinCost(0, days, costs, dp);
+        vector<int> dp(n+1, 0);
+        for(int i=n-1; i>=0; i--){
+            // get 1 day pass
+            int pass1 = costs[0] + dp[i+1];
+            // get 7 day pass
+            auto it = lower_bound(days.begin(), days.end(), days[i]+7);
+            int nextDayindex = it - days.begin();
+            int pass7 = costs[1] + dp[nextDayindex];
+            // get 30 day pass
+            it = lower_bound(days.begin(), days.end(), days[i]+30); 
+            nextDayindex = it - days.begin();
+            int pass30 = costs[2] + dp[nextDayindex];
+            
+            dp[i] = min(pass1, min(pass7, pass30));
+        }
+        return dp[0];
     }
 };
