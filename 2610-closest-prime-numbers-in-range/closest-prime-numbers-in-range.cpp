46f7cb23f0ca874783163ad6f331b23905093787
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2025-03-07 22:30:21 +0530

    Time: 240 ms (40.92%) | Memory: 14.1 MB (74.07%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,24 +1,22 @@
+typedef long long LL;
 class Solution {
 public:
     vector<int> closestPrimes(int left, int right) {
         vector<bool> isPrime(right+1, true);
-        isPrime[0] = isPrime[1] = false;
         int p1 = -1, p2 = -1;
-        for(int i=2; i*i<=right; i++){
+        for(int i=2, prevPrime=-1; i<=right; i++){
             if(!isPrime[i])   continue;
-            for(int j=i*i; j<=right; j+=i)
-                isPrime[j] = false;
-        }
-        for(int i=left, prevPrime=-1; i<=right; i++){
-            if(isPrime[i]){
+            if(i>=left){
                 if(p1 == -1)    p1 = i;
                 else if(p2 == -1)    p2 = i;
-                else if((i - prevPrime) < (p2 - p1)){
+                else if(i - prevPrime < p2 - p1){
                     p1 = prevPrime;
                     p2 = i;
                 }
                 prevPrime = i;
             }
+            for(LL j=(LL)i*i; j<=right; j+=i)
+                isPrime[j] = false;
         }
         if(p1 != -1 && p2 != -1)    return {p1, p2};
         return {-1, -1};
