dcd9950545e8c188c4b2f8f3fc71d8f0e9b0713a
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2025-04-26 14:29:42 +0530

    Time: 194 ms (5.15%) | Memory: 147.2 MB (5.15%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
@@ -1,59 +1,35 @@
+typedef long long LL;
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        long long ct = 0;
-        int n = nums.size(), minInd=-1, maxInd=-1, oobInd=-1;
-        for(int i=0; i<n; i++){
-            if(nums[i] == minK) minInd = i;
-            if(nums[i] == maxK) maxInd = i;
-            if(nums[i] < minK || maxK < nums[i])    oobInd = i;
-            if(minInd != -1 && maxInd != -1){
-                int low = min(minInd, maxInd);
-                if(low - oobInd >= 0){
-                    ct += low - oobInd;
-                }
-            }
+        LL cnt;
+        int i, j, n = nums.size();
+        map<int,int> freq;
+        vector<int> nextOutOfRangeInd(n,n);
+        i = j = cnt = 0;
+        int outOfRangeIndex = n;
+        for(int i=n-1; i>=0; i--){
+            nextOutOfRangeInd[i] = outOfRangeIndex;
+            outOfRangeIndex = nums[i] < minK || nums[i] > maxK ? i : outOfRangeIndex;
         }
-        return ct;
+        while(j<n){
+            freq[nums[j]] ++;
+            auto maxIt = *freq.rbegin();
+            auto minIt = *freq.begin();
+            if(maxIt.first > maxK || minIt.first < minK){
+                i = ++j;
+                freq.clear();
+                continue;
+            }
+            while(freq[minK] > 0 && freq[maxK] > 0){
+                cnt += (LL) nextOutOfRangeInd[j] - j;
+                if(--freq[nums[i]] == 0)
+                    freq.erase(nums[i]);
+                i++; 
+            }
+            j ++;
+        }
+        return cnt;
 
-        // wrong
-        // int n = nums.size();
-        // long long ct = 0;
-        // vector<int> mins, maxs, outs, minsmaxsouts={-1};
-        // for(int i=0; i<n; i++){
-        //     bool flag = false;
-        //     if(nums[i]==minK){
-        //         mins.push_back(i);
-        //         flag = true;
-        //     }
-        //     if(nums[i]==maxK){
-        //         maxs.push_back(i);
-        //         flag = true;
-        //     }
-        //     if(minK > nums[i] || maxK < nums[i]){
-        //         outs.push_back(i);
-        //         flag = true;
-        //     }
-        //     if(flag)    minsmaxsouts.push_back(i);
-        // }
-        // outs.push_back(n);
-        // int i=0, j=0;
-        // while(i<mins.size() && j<maxs.size()){
-        //     int low = min(mins[i], maxs[j]);
-        //     int high = max(mins[i], maxs[j]);
-        //     if(*(lower_bound(minsmaxsouts.begin(), minsmaxsouts.end(), low) + 1) == high || low==high){
-        //         int l = minsmaxsouts[lower_bound(minsmaxsouts.begin(), minsmaxsouts.end(), low) - minsmaxsouts.begin() - 1];
-        //         int h = *upper_bound(outs.begin(), outs.end(), high);
-        //         ct += (long long) (low - l) * (h - high);
-        //     }
-        //     if(high == low){
-        //         i++;
-        //         j++;
-        //         continue;
-        //     }
-        //     if(mins[i]==low)    i++;
-        //     else                j++;
-        // }
-        // return ct;
     }
 };
