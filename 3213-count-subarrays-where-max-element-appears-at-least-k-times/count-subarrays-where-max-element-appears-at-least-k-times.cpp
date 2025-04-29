64925cfcfbfba12ee29b733efd91209547abc9e3
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2025-04-29 15:00:08 +0530

    Time: 8 ms (36.17%) | Memory: 121.7 MB (74.24%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,18 +1,21 @@
+typedef long long LL;
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int k) {
-        long long ct = 0;
-        vector<int> maxPos;
-        int n = nums.size(), maxElem = *max_element(nums.begin(),nums.end());
-        for(int i=0; i<n; i++){
-            if(nums[i]==maxElem)
-                maxPos.push_back(i);
+        LL cntOfValidSubarrays = 0;
+        int maxElem = *max_element(nums.begin(), nums.end());
+        int cntOfMaxElem = 0, n = nums.size(), i, j;
+        cntOfMaxElem = i = j = 0;
+        while(j<n){
+            if(nums[j] == maxElem)  cntOfMaxElem ++;
+            while(cntOfMaxElem == k){
+                cntOfValidSubarrays += (LL) n - j;
+                if(nums[i] == maxElem)  cntOfMaxElem --;
+                i ++;
+            }
+            j ++;
         }
-        for(int i=0, j=k-1; j<maxPos.size(); i++,j++){
-            int l = maxPos[i]+1, r = n-maxPos[j];
-            if(i!=0) l = maxPos[i] - maxPos[i-1];
-            ct += (long long) l * r;
-        }
-        return ct;
+        return cntOfValidSubarrays;
     }
+
 };
