87474984b68eb16764b7859cf667d484c432e33c
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2025-06-03 11:15:11 +0530

    Time: 6 ms (46.15%) | Memory: 44.3 MB (62.50%) - LeetSync

--- 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
     int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
-        int candiesCollected = 0, n = status.size();
+        int n = status.size();
+        int candiesCollected = 0;
         queue<int> Q;
         vector<int> vis(n, 0);
-        
-        for(auto box: initialBoxes){
-            vis[box] = 1;
-            if(status[box] == 1){
-                Q.push(box);
-                status[box] = 2;
+        for(auto initialBox: initialBoxes){
+            vis[initialBox] = 1;
+            if(status[initialBox] == 1){
+                Q.push(initialBox);
+                status[initialBox] = 2;
             }
         }
         while(Q.size() > 0){
-            int currBox = Q.front();
-            candiesCollected += candies[currBox];
+            int box = Q.front();
+            candiesCollected += candies[box];
             Q.pop();
 
-            for(auto key: keys[currBox]){
-                if(status[key] == 2)    continue;
-                status[key] = 1;
-                if(vis[key] == 1){
-                    Q.push(key); 
-                    status[key] = 2;
-                }   
-            } 
-            for(auto containedBox: containedBoxes[currBox]){
+            for(auto key: keys[box]){
+                if(!vis[key]) {
+                    status[key] = 1;
+                    continue;
+                }
+                if(status[key] == 2) continue;
+                Q.push(key);
+                status[key] = 2;
+            }
+
+            for(auto containedBox: containedBoxes[box]){
                 vis[containedBox] = 1;
                 if(status[containedBox] == 1){
                     Q.push(containedBox);
                     status[containedBox] = 2;
                 }
-            }      
+            }
+            
         }
         return candiesCollected;
     }
