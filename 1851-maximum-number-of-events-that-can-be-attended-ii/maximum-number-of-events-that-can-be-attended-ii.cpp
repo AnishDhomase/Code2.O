7dc948130d7f3696750461f57f59e3a31ded097d
Anish Dhomase <127585174+AnishDhomase@users.noreply.github.com>
2025-07-08 14:58:22 +0530

    Time: 157 ms (56.61%) | Memory: 123.6 MB (57.50%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,40 +1,35 @@
 class Solution {
-    int getNextPossibleEvent(int startInd, int prevEndDay, vector<vector<int>>& events){
+    int getNextInd(int i, int endOfPrevEvent, vector<vector<int>>& events){
         int n = events.size();
-        int low = startInd, high = n - 1;
-        int nextPossibleStart = n;
+        int low = i, high =  n - 1, nextInd = n;
         while(low <= high){
-            int mid = low + (high-low) / 2;
-            int startDayOfEvent = events[mid][0];
-            if(startDayOfEvent <= prevEndDay)
-                low = mid + 1;
-            else{
-                nextPossibleStart = mid;
+            int mid = low + (high-low)/2;
+            if(endOfPrevEvent < events[mid][0]){
+                nextInd = mid;
                 high = mid - 1;
             }
-        }
-        return nextPossibleStart;
+            else    low = mid + 1;
+        } 
+        return nextInd;
     }
-    int getMaxVal(int i, int k, vector<vector<int>>& events, vector<vector<int>> &dp){
+    int getMaxValue(int i, int k, vector<vector<int>>& events, vector<vector<int>> &dp){
         int n = events.size();
-        if(k == 0 || i == n)    return 0;
+        if(i == n || k == 0)  return 0;
         if(dp[i][k] != -1)  return dp[i][k];
 
-        // ignore event
-        int dontdoIt = getMaxVal(i+1, k, events, dp);
+        // don't attend
+        int dontAttend = getMaxValue(i+1, k, events, dp);
 
-        // do event
-        int startDay = events[i][0], endDay = events[i][1], val = events[i][2];
-        int nextEventInd = getNextPossibleEvent(i+1, endDay, events);
-        int doIt = val + getMaxVal(nextEventInd, k-1, events, dp);
-
-        return dp[i][k] = max(dontdoIt, doIt);
+        // attend
+        int nextInd = getNextInd(i+1, events[i][1], events);
+        int attend = events[i][2] + getMaxValue(nextInd, k-1, events, dp);
+        return dp[i][k] = max(attend, dontAttend);
     }
 public:
     int maxValue(vector<vector<int>>& events, int k) {
         int n = events.size();
         vector<vector<int>> dp(n, vector<int>(k+1, -1));
         sort(events.begin(), events.end());
-        return getMaxVal(0, k, events, dp);
+        return getMaxValue(0, k, events, dp);
     }
 };
